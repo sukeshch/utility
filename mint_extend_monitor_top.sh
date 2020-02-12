250bc50c284fa3b22e334d7be5525520ebb19784
Sukesh Cheripalli <scheripa@visteon.com>
2020-02-12 13:26:24 +0530

    script to extend full hd and 4k monitor with same resolution

---
+++ mint_extend_monitor_top.sh
@@ -0,0 +1,2 @@
+xrandr --fb 3840x4320 --output DP-1-3 --mode 1920x1080 --scale 2x2 --panning 3840x2160+0+0 --pos 0x0 --above eDP-1-1 
+xrandr --output eDP-1-1 --auto --panning 3840x2160+0+2160 --pos 0x2160 --primary
